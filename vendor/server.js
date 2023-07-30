45b0d2c62e97d2e15d3c6a4b13bb90191a3222ca
Helmi Trisna <helmytrisna86@gmail.com>
2023-07-30 12:55:56 +0800

    deploy vercel

--- vendor/server.js
+++ vendor/server.js
@@ -28,10 +28,7 @@ app.use(express.json());
 app.use(auth);
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 app.get("/public/:mimeType/:fileName", publicFile);
